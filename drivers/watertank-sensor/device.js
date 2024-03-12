3e60fd0ee7436412c3443c071f597d0f9d824122
Johan Rossouw <johan@electric-shed.com.au>
2024-03-12 18:58:13 +0800

    Hopefully fixed multiple device setting capabilities

--- drivers/watertank-sensor/device.js
+++ drivers/watertank-sensor/device.js
@@ -7,22 +7,23 @@ class VirtualDevice extends Homey.Device {
     this.log(`Virtual Device (${this.getName()}) initialized`);
     const actionDepth = this.homey.flow.getActionCard('water_sensor_water_level');
     actionDepth.registerRunListener(async (args, state) => {
-      this.setCapabilityValue('measure_water_level', args.water_level.toFixed(2) * 1.0);
+      this.log('Setting water level for device ', this);
+      args.device.setCapabilityValue('measure_water_level', args.water_level.toFixed(2) * 1.0);
     });
     const actionVolume = this.homey.flow.getActionCard('water_sensor_volume');
     actionVolume.registerRunListener(async (args, state) => {
-      this.setCapabilityValue('measure_volume', args.volume.toFixed() * 1.0);
+      args.device.setCapabilityValue('measure_volume', args.volume.toFixed() * 1.0);
     });
     const actionCapacity = this.homey.flow.getActionCard('water_sensor_capacity');
     actionCapacity.registerRunListener(async (args, state) => {
-      this.setCapabilityValue('measure_capacity', args.capacity.toFixed(1) * 1.0);
+      args.device.setCapabilityValue('measure_capacity', args.capacity.toFixed(1) * 1.0);
     });
     const actionAlarm = this.homey.flow.getActionCard('water_sensor_low_level_alarm');
     actionAlarm.registerRunListener(async (args, state) => {
       this.log('Arguments:', args);
       const alarmState = args.alarm_setting === 'on';
       this.log('Alarm state: ', alarmState);
-      this.setCapabilityValue('alarm_low_level', alarmState);
+      args.device.setCapabilityValue('alarm_low_level', alarmState);
     });
   }
 
